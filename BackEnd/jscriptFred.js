faeda5a6c0e36c0467349b04dee8fdc0105fa8b1
Frederic Guillet <docteurmac@gmail.com>
2021-09-05 15:33:11 +0200

    Partie en commentaire pour Lundi

--- BackEnd/jscriptFred.js
+++
@@ -1,2 +0,0 @@
-var element = document.getElementById("5be9c8541c9d440000665243");
-alert(element);
---
+++ jscriptFred.js
@@ -0,0 +1,50 @@
+const { readlink } = require("fs");
+
+var element = document.getElementById("5be9c8541c9d440000665243");
+
+// Partie lien entre le front et le BackEnd
+
+// Bon visiblement la ligne du dessus ne sert a rien 
+// il faut que je trouve ou est le pont entre le fichier JSON ou l'url du server
+
+
+
+// Partie index.html
+
+// il faudrait une sorte de constructor pour me créeer un Object complet
+
+// puis lui donner donner un style un peu sympa
+// Puis utiiser le Css ou le style en java
+
+// Une fois tout cela il faut que je lui fasse faire une boucle for pour rappatrier tous les teddys
+
+// Ensuite l'affichage des différents éléments instancié
+
+// Et régler en BootStrap pour un bel affichage en responsive avec md sd etc ....
+
+// Tout cela c'est pour la partie index
+
+
+
+
+// Partie image du produit
+
+// Voir si un simple javascript qui existe partout pour le voir en grand avec une croix pour la fermeture
+// Ou dois je faire une vrai page que pour le produit
+
+// Je suppose qu'il faudra utiliser une sorte de GetSeleted item id
+
+
+
+
+
+// Partie Panier de commande
+
+// Récupérer les différents produits cochés avec éventuellement leur couleur et surtout la quantité
+// Mettre en place un mini formulaire de confirmation avec coordonnées
+
+
+
+// Partie Confirmation
+
+// Afficher une page lambda pour féliciter le client et mettre le récap de sa commande
