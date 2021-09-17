37323f25bb540c21b86eaea36eaac9756bbcf02a
Frederic Guillet <docteurmac@gmail.com>
2021-09-17 16:44:57 +0200

    Propre

--- FrontEnd/produits.js
+++ FrontEnd/produits.js
@@ -15,7 +15,7 @@ fetch('http://localhost:3000/api/teddies/' + id)
 
     // on crée nos variables
     let nomDuNounours = document.createElement("H2");
-    let imageDuNounours = document.createElement("img");
+    let imageDuNounoursDetail = document.createElement("img");
     let prixDuNounours = document.createElement("H3");
     let couleurDuNounours = document.createElement("select")
     let descriptionDuNounours = document.createElement("p")
@@ -25,7 +25,7 @@ fetch('http://localhost:3000/api/teddies/' + id)
 
     // Pour le décorer en css
     nomDuNounours.setAttribute("class", "nounours");
-    imageDuNounours.setAttribute("class", "imageDuNounours");
+    imageDuNounoursDetail.setAttribute("class", "imageDuNounoursDetail");
     prixDuNounours.setAttribute("class", "nounours");
     couleurDuNounours.setAttribute("class", "nounours");
     descriptionDuNounours.setAttribute("class", "descriptionDuNounours");
@@ -34,7 +34,7 @@ fetch('http://localhost:3000/api/teddies/' + id)
 
     // on les cherches
     nomDuNounours.textContent = _id.name;
-    imageDuNounours.setAttribute("src", _id.imageUrl);
+    imageDuNounoursDetail.setAttribute("src", _id.imageUrl);
     prixDuNounours.textContent =  _id.price;
 
     prixDuNounours.textContent = prixDuNounours.textContent / 100 + " €";
@@ -43,7 +43,7 @@ fetch('http://localhost:3000/api/teddies/' + id)
 
     // on les affiches
     carteNounoursDetail.appendChild(nomDuNounours);
-    carteNounoursDetail.appendChild(imageDuNounours);
+    carteNounoursDetail.appendChild(imageDuNounoursDetail);
     carteNounoursDetail.appendChild(prixDuNounours, );
     carteNounoursDetail.appendChild(couleurDuNounours);
     carteNounoursDetail.appendChild(descriptionDuNounours);
